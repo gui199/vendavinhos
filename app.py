ecfec241c7c27dde43d3da7cca7d6b99fbc973f4
gui1984 <gui1@ymail.com>
2021-05-19 22:55:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,76 +17,80 @@ API com cadastro de clientes
 
 #importação dos módulos
 import os
-from flask import Flask, jsonify
 import json
+from flask import Flask, jsonify
 import pandas as pd
 import requests
 
 #definição de constantes, sites com json
 #cadastro de clientes
-site_cli = "http://www.mocky.io/v2/598b16291100004705515ec5"
+SITE_CLI = "http://www.mocky.io/v2/598b16291100004705515ec5"
 #histórico de compras
-site_hc =  "http://www.mocky.io/v2/598b16861100004905515ec7"
+SITE_HC = "http://www.mocky.io/v2/598b16861100004905515ec7"
 
-#funcao de requisicao de dados, 
-#transforma a requisicao json em um dataframe(tabela) para facilitar a 
+#funcao de requisicao de dados,
+#transforma a requisicao json em um dataframe(tabela) para facilitar a
 #manipulação de dados, foi usado a biblioteca pandas pois permite transformar
 #json em dados tabular para melhor trabalhar e fazer busca e atualizações
 def get_dados(site):
     #fazer a requisição do site
-    r = requests.get(site, timeout=15)
+    req = requests.get(site, timeout=15)
     #transferir o json para uma variavel
-    jason=r.json()
+    jason = req.json()
     #fechar a requisicção
-    r.close()
+    req.close()
     #transformar json em dataframe
     dframe_temp = pd.DataFrame(jason)
     return dframe_temp
- 
+
 # 1 - Liste os clientes ordenados pelo maior valor total em compras.
 #função que retorna a atividade pedida
 def get_lista_orden_cli():
     #variavel que armazena o dataframe dos clientes
-    dframe_cli = get_dados(site_cli)
+    dframe_cli = get_dados(SITE_CLI)
     #variavel que armazena o dataframe do histórico de compras dos clientes
-    dframe_hc = get_dados(site_hc)
+    dframe_hc = get_dados(SITE_HC)
     # corrigir o cpf errado do cliente
     dframe_hc.loc[dframe_hc['cliente'] == '000.000.000.01', 'cliente'] = "0000.000.000.01"
     #lista vazia para armazenar os maiores valores total em compras
     novalista = []
     #loop para encontrar o maior valor para cada cliente
-    for x in range(len(dframe_cli)):
-        valor = round(dframe_hc[dframe_hc['cliente']== "0"+str(dframe_cli.iloc[x]["cpf"]).replace("-",".")]['valorTotal'].sum(), 2)
-        novalista.append(valor)      
+    for client_maior_valor in range(len(dframe_cli)):
+        valor = round(
+            dframe_hc[dframe_hc['cliente'] == \
+                      "0" + str(dframe_cli.iloc[client_maior_valor]["cpf"]).replace(
+                          "-", ".")]['valorTotal'].sum(), 2)
+        novalista.append(valor)
     #criação de uma nova coluna para cada cliente com o maior valor total em compras encontrado
-    dframe_cli['valorTotal'] = novalista      
+    dframe_cli['valorTotal'] = novalista
     #organizar a tabela em ordem descendente(maior para menor)
-    dframe_cliente = dframe_cli.sort_values('valorTotal', ascending=False)  
+    dframe_cliente = dframe_cli.sort_values('valorTotal', ascending=False)
     #retornar a tabela
     return dframe_cliente
-    
+
 # 2 - Mostre o cliente com maior compra única no último ano (2016).
-#função que retorna a atividade pedida    
+#função que retorna a atividade pedida
 def get_maior_compra(ano):
     #variavel que armazena o dataframe dos clientes
-    dframe_cli = get_dados(site_cli)
+    dframe_cli = get_dados(SITE_CLI)
     #variavel que armazena o dataframe do histórico de compras dos clientes
-    dframe_hc = get_dados(site_hc)
+    dframe_hc = get_dados(SITE_HC)
     # corrigir o cpf errado do cliente
-    dframe_hc.loc[dframe_hc['cliente'] == '000.000.000.01', 'cliente'] = "0000.000.000.01"
+    dframe_hc.loc[dframe_hc['cliente'] == '000.000.000.01', 'cliente'] = \
+    "0000.000.000.01"
     #transforma a variavel recebida em string
     ano = str(ano)
     #cria-se um novo dataframe com somente o ano desejado
-    ddt = dframe_hc[dframe_hc["data"].str.contains(ano)]  
+    ddt = dframe_hc[dframe_hc["data"].str.contains(ano)]
     #encontra-se o  cliente_maior_compra
-    cmp=ddt[ddt['valorTotal']==ddt['valorTotal'].max()]['cliente'].array[0]
+    cmp = ddt[ddt['valorTotal'] == ddt['valorTotal'].max()]['cliente'].array[0]
     #modfificar a string para o seu formato correspondente na outra tabela(cliente)
-    cmp=cmp[::-1].replace(".","-",1)[::-1]    
+    cmp = cmp[::-1].replace(".", "-", 1)[::-1]
     #buscar o perfi/dados do cliente desejado
     cliente_maior_compra = dframe_cli[dframe_cli['cpf'] == cmp[1:]]
     #retorna a linha/row com os dados do cliente
     return cliente_maior_compra
-    
+
 
 
 '''
@@ -102,19 +106,22 @@ def home():
     #string contendo uma página html,
     #links para as api requisitadas
     return '''<h1>Home</h1>
-    <p><a href="/api/v1/recursos/clientes/all" rel="gallery"> 1 - Liste os clientes ordenados pelo maior valor total em compras</a></p>
-    <p><a href="/api/v1/recursos/clientes/maiorcompra/2016" rel="gallery"> 2 - Mostre o cliente com maior compra única no último ano (2016).</a></p>'''
+    <p><a href="/api/v1/recursos/clientes/all" rel="gallery"> 
+    1 - Liste os clientes ordenados pelo maior valor total em compras</a></p>
+    <p><a href="/api/v1/recursos/clientes/maiorcompra/2016" rel="gallery"> 2 
+    - Mostre o cliente com maior compra única no último ano (2016).</a></p>'''
 
 #página de erro
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(error):
     #string contendo uma página html,
     return "<h1>404</h1><p>Página não encontrada.</p>", 404
 
 # 1 - Liste os clientes ordenados pelo maior valor total em compras.
 @app.route('/api/v1/recursos/clientes/all', methods=['GET'])
 def api_all():
-    #variavel que armazena a lista/tabela com os clientes ordenados pelo maior valor total em compras
+    # variavel que armazena a lista/tabela com os clientes ordenados pelo
+    # maior valor total em compras
     cli_ordem = get_lista_orden_cli().to_json(orient="records")
     #trasnformação da var em formato json
     parsed = json.loads(cli_ordem)
@@ -127,9 +134,9 @@ def api_maior(name):
     #teste para verificar se a variavel passada é um valor numerico
     #e se o valor encontra-se entre 2014 e 2016
     #caso contrario retorna hello world
-    if name.isnumeric() and int(name) >=2014 and int(name) <=2016:
+    if name.isnumeric() and int(name) >= 2014 and int(name) <= 2016:
         pass
-    else:#        
+    else:#
         return {'hello': 'world'}
     #variavel que armazena a lista/tabela com o cliente com maior compra única no  ano especificado
     cli_maior = get_maior_compra(name).to_json(orient="records")
@@ -138,6 +145,6 @@ def api_maior(name):
 
 #processo principal
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
+    PORT = int(os.environ.get("PORT", "5000"))
     #iniciar aplicativo
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=PORT, threaded=True)
